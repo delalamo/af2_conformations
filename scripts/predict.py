729e35760efbed569442a387c7138e9f90ee2f13
Diego del Alamo <diego.delalamo@gmail.com>
2022-01-31 00:44:22 +0100

    Beautifying

--- scripts/predict.py
+++ scripts/predict.py
@@ -13,6 +13,7 @@ from typing import Any, List, Mapping, NoReturn
 
 from absl import logging
 
+
 def set_config(
     use_templates: bool,
     max_msa_clusters: int,
@@ -22,135 +23,130 @@ def set_config(
     n_struct_module_repeats: int,
     n_features_in: int,
     monomer: bool = True,
-    model_params: int = 0
-  ) -> model.RunModel:
-  
-  r""" Generated Runner object for AlphaFold
-  
-  Parameters
-  ----------
-  use_templates : Whether templates are used
-  max_msa_cluster : How many sequences to use in MSA
-  max_extra_msa : How many extra sequences to use? Unclear
-  max_recycles : Number of recycling iterations
-  model_id : Which AF2 model to use
-  n_struct_module_repeats : Number of passes through structure module
-  n_features_in : Unclear
-  monomer : Predicting as a monomer (set to False if using AlphaFold-multimer)
-  model_params : Which AF2 model config to use
+    model_params: int = 0,
+) -> model.RunModel:
 
-  Returns
-  ----------
-  AlphaFold RunModel object
+    r"""Generated Runner object for AlphaFold
 
-  """  
+    Parameters
+    ----------
+    use_templates : Whether templates are used
+    max_msa_cluster : How many sequences to use in MSA
+    max_extra_msa : How many extra sequences to use? Unclear
+    max_recycles : Number of recycling iterations
+    model_id : Which AF2 model to use
+    n_struct_module_repeats : Number of passes through structure module
+    n_features_in : Unclear
+    monomer : Predicting as a monomer (set to False if using AlphaFold-multimer)
+    model_params : Which AF2 model config to use
 
-  if model_id not in range( 1, 6 ):
-    logging.warning( "model_id must be between 1 and 5!" )
-    if use_templates:
-        model_id = random.randint( 1, 2 )
-    else:
-        model_id = random.randint( 1, 5 )
+    Returns
+    ----------
+    AlphaFold RunModel object
 
-  # Match model_params to model_id
-  # Sometimes we don't want to do this, for example,
-  #   to reproduce output from ColabFold (which only uses models 1 and 3)
+    """
 
-  name = f"model_{ model_params }_ptm"
-  if not monomer:
-    name = f"model_{ model_params }_multimer"
+    if model_id not in range(1, 6):
+        logging.warning("model_id must be between 1 and 5!")
+        if use_templates:
+            model_id = random.randint(1, 2)
+        else:
+            model_id = random.randint(1, 5)
 
-  cfg = config.model_config( name )
+    # Match model_params to model_id
+    # Sometimes we don't want to do this, for example,
+    #   to reproduce output from ColabFold (which only uses models 1 and 3)
 
-  #### Provide config settings
+    name = f"model_{ model_params }_ptm"
+    if not monomer:
+        name = f"model_{ model_params }_multimer"
 
-  #### MSAs
+    cfg = config.model_config(name)
 
-  cfg.data.eval.num_ensemble = 1
-  if max_msa_clusters > 0:
-    cfg.data.eval.max_msa_clusters = min(
-        n_features_in, max_msa_clusters
-      )
-  if max_extra_msa > 0:
-    cfg.data.common.max_extra_msa = max( 1, min(
-        n_features_in - max_msa_clusters, max_extra_msa )
-      )
+    #### Provide config settings
 
-  #### Recycle and number of iterations
+    #### MSAs
 
-  if monomer:
-    cfg.data.common.num_recycle = max_recycles
-  cfg.model.num_recycle = max_recycles
-  cfg.model.heads.structure_module.num_layer = n_struct_module_repeats
+    cfg.data.eval.num_ensemble = 1
+    if max_msa_clusters > 0:
+        cfg.data.eval.max_msa_clusters = min(n_features_in, max_msa_clusters)
+    if max_extra_msa > 0:
+        cfg.data.common.max_extra_msa = max(
+            1, min(n_features_in - max_msa_clusters, max_extra_msa)
+        )
 
-  #### Templates
+    #### Recycle and number of iterations
 
-  t = use_templates # for brevity
+    if monomer:
+        cfg.data.common.num_recycle = max_recycles
+    cfg.model.num_recycle = max_recycles
+    cfg.model.heads.structure_module.num_layer = n_struct_module_repeats
 
-  cfg.data.common.use_templates = use_templates
-  cfg.model.embeddings_and_evoformer.template.embed_torsion_angles = t
-  cfg.model.embeddings_and_evoformer.template.enabled = t
-  cfg.data.common.reduce_msa_clusters_by_max_templates = t
-  cfg.data.eval.subsample_templates = t
+    #### Templates
 
-  p = data.get_model_haiku_params(
-      model_name=name, data_dir="."
+    t = use_templates  # for brevity
+
+    cfg.data.common.use_templates = use_templates
+    cfg.model.embeddings_and_evoformer.template.embed_torsion_angles = t
+    cfg.model.embeddings_and_evoformer.template.enabled = t
+    cfg.data.common.reduce_msa_clusters_by_max_templates = t
+    cfg.data.eval.subsample_templates = t
+
+    p = data.get_model_haiku_params(model_name=name, data_dir=".")
+
+    logging.debug("Prediction parameters:")
+    logging.debug("\tModel ID: {}".format(model_id))
+    logging.debug("\tUsing templates: {}".format(t))
+    logging.debug(
+        "\tMaximum MSA clusters: {}".format(cfg.data.eval.max_msa_clusters)
+    )
+    logging.debug(
+        "\tMaximum extra MSA clusters: {}".format(
+            cfg.data.common.max_extra_msa
+        )
+    )
+    logging.debug(
+        "\tNumber recycling iterations: {}".format(cfg.model.num_recycle)
+    )
+    logging.debug(
+        "\tNumber of structure module repeats: {}".format(
+            cfg.model.heads.structure_module.num_layer
+        )
     )
 
-  logging.debug( "Prediction parameters:" )
-  logging.debug( "\tModel ID: {}".format( model_id ) )
-  logging.debug( "\tUsing templates: {}".format( t ) )
-  logging.debug( "\tMaximum number of MSA clusters: {}".format(
-    cfg.data.eval.max_msa_clusters ) )
-  logging.debug( "\tMaximum number of extra MSA clusters: {}".format(
-    cfg.data.common.max_extra_msa ) )
-  logging.debug( "\tMaximum number of recycling iterations: {}".format(
-    cfg.model.num_recycle ) )
-  logging.debug( "\tMaximum number of structure module repeats: {}".format(
-    cfg.model.heads.structure_module.num_layer ) )
+    return model.RunModel(cfg, p)
 
-  return model.RunModel( cfg, p )
 
 def run_one_job(
-    runner: model.RunModel,
-    features_in: dict,
-    random_seed: int,
-    outname: str
-  ) -> Mapping[ str, Any ]:
-  r""" Runs one AF2 job with input parameters
+    runner: model.RunModel, features_in: dict, random_seed: int, outname: str
+) -> Mapping[str, Any]:
+    r"""Runs one AF2 job with input parameters
 
-  Parameters
-  ----------
-  runner : AlphaFold2 job runner
-  features_in : Input features, including MSA and templates
-  random_seed : Random seed
-  outname : Name of PDB file to write
+    Parameters
+    ----------
+    runner : AlphaFold2 job runner
+    features_in : Input features, including MSA and templates
+    random_seed : Random seed
+    outname : Name of PDB file to write
 
-  Returns
-  ----------
-  None
+    Returns
+    ----------
+    None
 
-  """
+    """
 
-  # Do one last bit of processing
-  features = runner.process_features(
-      features_in,
-      random_seed=random_seed
-    )
+    # Do one last bit of processing
+    features = runner.process_features(features_in, random_seed=random_seed)
 
-  # Generate the model
-  result = runner.predict( features, random_seed )
-  pred = protein.from_prediction( features, result )
+    # Generate the model
+    result = runner.predict(features, random_seed)
+    pred = protein.from_prediction(features, result)
 
-  # Write to file
-  to_pdb(
-      outname,
-      pred,
-      result[ 'plddt' ],
-      features_in[ 'residue_index' ]
-    )
+    # Write to file
+    to_pdb(outname, pred, result["plddt"], features_in["residue_index"])
 
-  return result
+    return result
+
 
 def predict_structure_from_templates(
     seq: str,
@@ -163,74 +159,64 @@ def predict_structure_from_templates(
     max_msa_clusters: int = -1,
     max_extra_msa: int = -1,
     max_recycles: int = 3,
-    n_struct_module_repeats: int = 8
-  ) -> NoReturn:
-  
-  r""" Predicts the structure.
+    n_struct_module_repeats: int = 8,
+) -> NoReturn:
 
-  Parameters
-  ----------
-  seq : Sequence
-  outname : Name of output PDB
-  a3m_lines : String of entire alignment
-  template_paths : Where to locate templates
-  model_id : Which AF2 model to run (must be 1 or 2 for templates)
-  model_params : Which parameters to provide to AF2 model
-  random_seed : Random seed
-  max_msa_clusters : Number of sequences to use
-  max_extra_msa : ???
-  max_recycles : Number of iterations through AF2
-  n_struct_module_repeats : Number of passes through structural refinement
-  move_prefix : Prefix for temporary files (deleted after fxn completion)
+    r"""Predicts the structure.
 
-  Returns
-  ----------
-  None
+    Parameters
+    ----------
+    seq : Sequence
+    outname : Name of output PDB
+    a3m_lines : String of entire alignment
+    template_paths : Where to locate templates
+    model_id : Which AF2 model to run (must be 1 or 2 for templates)
+    model_params : Which parameters to provide to AF2 model
+    random_seed : Random seed
+    max_msa_clusters : Number of sequences to use
+    max_extra_msa : ???
+    max_recycles : Number of iterations through AF2
+    n_struct_module_repeats : Number of passes through structural refinement
+    move_prefix : Prefix for temporary files (deleted after fxn completion)
 
-  """
+    Returns
+    ----------
+    None
 
-  if random_seed == -1:
-    random_seed = random.randrange( sys.maxsize )
+    """
 
-  if model_id not in ( 1, 2 ):
-    model_id = random.randint( 1, 2 )
+    if random_seed == -1:
+        random_seed = random.randrange(sys.maxsize)
 
-  if model_params not in ( 1, 2 ):
-    model_params = random.randint( 1, 2 )
+    if model_id not in (1, 2):
+        model_id = random.randint(1, 2)
 
-  # Assemble the dictionary of input features
-  features_in = util.setup_features(
-      seq,
-      a3m_lines,
-      util.mk_template(
-          seq,
-          a3m_lines,
-          template_path
-        ).features
+    if model_params not in (1, 2):
+        model_params = random.randint(1, 2)
+
+    # Assemble the dictionary of input features
+    features_in = util.setup_features(
+        seq, a3m_lines, util.mk_template(seq, a3m_lines, template_path).features
     )
 
-  # Run the models
-  model_runner = set_config(
-      True,
-      max_msa_clusters,
-      max_extra_msa,
-      max_recycles,
-      model_id,
-      n_struct_module_repeats,
-      len( features_in[ "msa" ] ),
-      model_params=model_params
+    # Run the models
+    model_runner = set_config(
+        True,
+        max_msa_clusters,
+        max_extra_msa,
+        max_recycles,
+        model_id,
+        n_struct_module_repeats,
+        len(features_in["msa"]),
+        model_params=model_params,
     )
 
-  result = run_one_job(
-      model_runner,
-      features_in,
-      random_seed,
-      outname
-    )
+    result = run_one_job(model_runner, features_in, random_seed, outname)
 
-  del model_runner
+    del model_runner
 
-  return result
+    return result
+
 
 def predict_structure_no_templates(
     seq: str,
@@ -242,102 +228,90 @@ def predict_structure_no_templates(
     max_msa_clusters: int = -1,
     max_extra_msa: int = -1,
     max_recycles: int = 3,
-    n_struct_module_repeats: int = 8
-  ) -> NoReturn:
-  
-  r""" Predicts the structure.
+    n_struct_module_repeats: int = 8,
+) -> NoReturn:
 
-  Parameters
-  ----------
-  seq : Sequence
-  outname : Name of output PDB
-  a3m_lines : String of entire alignment
-  model_id : Which AF2 model to run (must be 1 or 2 for templates)
-  random_seed : Random seed
-  max_msa_clusters : Number of sequences to use
-  max_extra_msa : ???
-  max_recycles : Number of iterations through AF2
-  n_struct_module_repeats : Number of passes through structural refinement
-  
-  Returns
-  ----------
-  None
+    r"""Predicts the structure.
 
-  """
+    Parameters
+    ----------
+    seq : Sequence
+    outname : Name of output PDB
+    a3m_lines : String of entire alignment
+    model_id : Which AF2 model to run (must be 1 or 2 for templates)
+    random_seed : Random seed
+    max_msa_clusters : Number of sequences to use
+    max_extra_msa : ???
+    max_recycles : Number of iterations through AF2
+    n_struct_module_repeats : Number of passes through structural refinement
 
-  # Set AF2 model details
-  if model_id not in range( 1, 6 ):
-    model_id = random.randint( 1, 5 )
+    Returns
+    ----------
+    None
 
-  if model_params not in range( 1, 6 ):
-    model_params = model_id
+    """
 
-  if random_seed == -1:
-    random_seed = random.randrange( sys.maxsize )
+    # Set AF2 model details
+    if model_id not in range(1, 6):
+        model_id = random.randint(1, 5)
 
-  features_in = util.setup_features(
-      seq,
-      a3m_lines,
-      util.mk_mock_template( seq )
+    if model_params not in range(1, 6):
+        model_params = model_id
+
+    if random_seed == -1:
+        random_seed = random.randrange(sys.maxsize)
+
+    features_in = util.setup_features(seq, a3m_lines, util.mk_mock_template(seq))
+
+    model_runner = set_config(
+        False,
+        max_msa_clusters,
+        max_extra_msa,
+        max_recycles,
+        model_id,
+        n_struct_module_repeats,
+        len(features_in["msa"]),
+        model_params=model_params,
     )
 
-  model_runner = set_config(
-      False,
-      max_msa_clusters,
-      max_extra_msa,
-      max_recycles,
-      model_id,
-      n_struct_module_repeats,
-      len( features_in[ "msa" ] ),
-      model_params=model_params
-    )
+    result = run_one_job(model_runner, features_in, random_seed, outname)
 
-  result = run_one_job(
-      model_runner,
-      features_in,
-      random_seed,
-      outname
-    )
+    del model_runner
 
-  del model_runner
+    return result
 
-  return result
 
 def to_pdb(
-    outname,
-    pred, # type unknown but check?
-    plddts, # type unknown but check?
-    res_idx
-  ) -> NoReturn:
-  
-  r""" Writes unrelaxed PDB to file
+    outname, pred, plddts, res_idx  # type unknown but check?  # type unknown but check?
+) -> NoReturn:
 
-  Parameters
-  ----------
-  outname : Name of output PDB
-  pred : Prediction to write to PDB
-  plddts : Predicted errors
-  res_idx : Residues to print (default=all)
-  
-  Returns
-  ----------
-  None
+    r"""Writes unrelaxed PDB to file
 
-  """
- 
-  with open( outname, 'w' ) as outfile:
-    outfile.write( protein.to_pdb( pred ) )
+    Parameters
+    ----------
+    outname : Name of output PDB
+    pred : Prediction to write to PDB
+    plddts : Predicted errors
+    res_idx : Residues to print (default=all)
 
-  with open( f"b_{ outname }", "w" ) as outfile:
-    for line in open( outname, "r" ).readlines():
-      if line[ 0:6 ] == "ATOM  ":
-        seq_id = int( line[ 22:26 ].strip() ) - 1
-        seq_id = np.where( res_idx == seq_id )[ 0 ][ 0 ]
-        outfile.write( "{}A{}{:6.2f}{}".format(
-            line[ :21 ],
-            line[ 22:60 ],
-            plddts[ seq_id ],
-            line[ 66: ] )
-        )
+    Returns
+    ----------
+    None
 
-  os.rename( f"b_{ outname }", outname )
+    """
+
+    with open(outname, "w") as outfile:
+        outfile.write(protein.to_pdb(pred))
+
+    with open(f"b_{ outname }", "w") as outfile:
+        for line in open(outname, "r").readlines():
+            if line[0:6] == "ATOM  ":
+                seq_id = int(line[22:26].strip()) - 1
+                seq_id = np.where(res_idx == seq_id)[0][0]
+                outfile.write(
+                    "{}A{}{:6.2f}{}".format(
+                        line[:21], line[22:60], plddts[seq_id], line[66:]
+                    )
+                )
+
+    os.rename(f"b_{ outname }", outname)
--- scripts/util.py
+++ scripts/util.py
@@ -7,186 +7,174 @@ from alphafold.data import pipeline
 from alphafold.data import templates
 from alphafold.data.tools import hhsearch
 
-def mk_mock_template(
-    seq: str
-  ) -> dict:
-  
-  r"""Generates mock templates that will not influence prediction
-  Taken from ColabFold version 62d7558c91a9809712b022faf9d91d8b183c328c
 
-  Parameters
-  ----------
-  seq: Query sequence
+def mk_mock_template(seq: str) -> dict:
 
-  Returns
-  ----------
-  Dictionary with blank/empty/meaningless features
+    r"""Generates mock templates that will not influence prediction
+    Taken from ColabFold version 62d7558c91a9809712b022faf9d91d8b183c328c
 
-  """
+    Parameters
+    ----------
+    seq: Query sequence
 
-  # Define constants
-  lentype = templates.residue_constants.atom_type_num
-  lseq = len( seq )
+    Returns
+    ----------
+    Dictionary with blank/empty/meaningless features
 
-  # Since alphafold's model requires a template input
-  # We create a blank example w/ zero input, confidence -1
-  aatypes = np.array(
-      templates.residue_constants.sequence_to_onehot(
-          "-" * lseq,
-          templates.residue_constants.HHBLITS_AA_TO_ID
+    """
+
+    # Define constants
+    lentype = templates.residue_constants.atom_type_num
+    lseq = len(seq)
+
+    # Since alphafold's model requires a template input
+    # We create a blank example w/ zero input, confidence -1
+    aatypes = np.array(
+        templates.residue_constants.sequence_to_onehot(
+            "-" * lseq, templates.residue_constants.HHBLITS_AA_TO_ID
         )
     )
 
-  return {
-      'template_all_atom_positions': np.zeros( ( lseq, lentype, 3 ) )[ None ],
-      'template_all_atom_masks': np.zeros( ( lseq, lentype ) )[ None ],
-      'template_sequence': [ f'none'.encode() ],
-      'template_aatype': aatypes[ None ],
-      'template_confidence_scores': np.full( lseq, -1 )[ None ],
-      'template_domain_names': [ f'none'.encode() ],
-      'template_release_date': [ f'none'.encode() ]
-  }
+    return {
+        "template_all_atom_positions": np.zeros((lseq, lentype, 3))[None],
+        "template_all_atom_masks": np.zeros((lseq, lentype))[None],
+        "template_sequence": [f"none".encode()],
+        "template_aatype": aatypes[None],
+        "template_confidence_scores": np.full(lseq, -1)[None],
+        "template_domain_names": [f"none".encode()],
+        "template_release_date": [f"none".encode()],
+    }
+
 
 ###############################
 
-def mk_template(
-    seq: str,
-    a3m_lines = str,
-    path = str
-  ) -> dict:
-  
-  r""" Parses templates into features
 
-  Parameters
-  ----------
-  seq : Query sequence
-  a3m_lines : Lines form MMSeqs2 alignment
-  path : Path to templates fetched using MMSeqs2
+def mk_template(seq: str, a3m_lines=str, path=str) -> dict:
 
-  Returns
-  ----------
-  Dictionary with features
+    r"""Parses templates into features
 
-  """
+    Parameters
+    ----------
+    seq : Query sequence
+    a3m_lines : Lines form MMSeqs2 alignment
+    path : Path to templates fetched using MMSeqs2
 
-  result = hhsearch.HHSearch(
-      binary_path="hhsearch",
-      databases=[ f"{ path }/pdb70" ]
-  ).query( a3m_lines )
+    Returns
+    ----------
+    Dictionary with features
 
-  return templates.HhsearchHitFeaturizer(
-      mmcif_dir=path,
-      max_template_date="2100-01-01",
-      max_hits=20,
-      kalign_binary_path="kalign",
-      release_dates_path=None,
-      obsolete_pdbs_path=None
-    ).get_templates(
-      query_sequence=seq,
-      hits=pipeline.parsers.parse_hhr( result )
-    )
+    """
+
+    result = hhsearch.HHSearch(
+        binary_path="hhsearch", databases=[f"{ path }/pdb70"]
+    ).query(a3m_lines)
+
+    return templates.HhsearchHitFeaturizer(
+        mmcif_dir=path,
+        max_template_date="2100-01-01",
+        max_hits=20,
+        kalign_binary_path="kalign",
+        release_dates_path=None,
+        obsolete_pdbs_path=None,
+    ).get_templates(query_sequence=seq, hits=pipeline.parsers.parse_hhr(result))
+
 
 ###############################
 
-def setup_features(
-    seq: str,
-    a3m_lines: list,
-    tfeatures_in: dict
-  ) -> dict:
-  
-  r""" Set up features for alphafold
 
-  Parameters
-  ----------
-  seq : Sequence (string)
-  a3m_lines : Sequence alignment lines
-  tfeatures_in : Template features
+def setup_features(seq: str, a3m_lines: list, tfeatures_in: dict) -> dict:
 
-  Returns
-  ----------
-  Alphafold features object
+    r"""Set up features for alphafold
 
-  """
+    Parameters
+    ----------
+    seq : Sequence (string)
+    a3m_lines : Sequence alignment lines
+    tfeatures_in : Template features
 
-  msa = pipeline.parsers.parse_a3m( a3m_lines )
-  return {
-      **pipeline.make_sequence_features(
-            sequence = seq,
-            description = "none",
-            num_res = len( seq )
-          ), **pipeline.make_msa_features(
-            msas = [ msa ]
-          ), **tfeatures_in
-        }
+    Returns
+    ----------
+    Alphafold features object
+
+    """
+
+    msa = pipeline.parsers.parse_a3m(a3m_lines)
+    return {
+        **pipeline.make_sequence_features(
+            sequence=seq, description="none", num_res=len(seq)
+        ),
+        **pipeline.make_msa_features(msas=[msa]),
+        **tfeatures_in,
+    }
+
 
 def mutate_msa(
     a3m_lines: str,
-    pos_res: Dict[ int, str ],
-  ) -> str:
-  r""" Mutates every position in an MSA to a residue of interest
-  
-  Example usage: mutate_msa( a3m_lines, { 15: "A", 155: "A" } )
-  This will mutate residues 15 and 155 to alanine throughout the MSA
+    pos_res: Dict[int, str],
+) -> str:
+    r"""Mutates every position in an MSA to a residue of interest
 
-  Parameters
-  ----------
-  a3m_lines : Sequence alignment
-  pos : Position to change
-  target_res : Residue to mutate to
+    Example usage: mutate_msa( a3m_lines, { 15: "A", 155: "A" } )
+    This will mutate residues 15 and 155 to alanine throughout the MSA
 
-  Returns
-  ----------
-  Sequence alignment (as string)
+    Parameters
+    ----------
+    a3m_lines : Sequence alignment
+    pos : Position to change
+    target_res : Residue to mutate to
 
-  """
+    Returns
+    ----------
+    Sequence alignment (as string)
 
-  for target_res in pos_res.values():
-    assert len( target_res ) == 1
+    """
+
+    for target_res in pos_res.values():
+        assert len(target_res) == 1
+
+    output = []
+
+    # Iterate over alignment lines
+    for line in a3m_lines.split("\n"):
+        if line.startswith(">"):
+            output.append(line)
+        elif len(line) > 1:
+            line = list(line)
+            for pos, res in pos_res.items():
+                if line[pos] in "ACDEFGHIKLMNPQRSTVWY":
+                    line[pos] = res
+            output.append("".join(line))
+        else:
+            output.append(line)
+    return "\n".join(output)
 
 
-  output = []
+def mutate(x, y):
+    mutate_msa(x, y)  # Alias for brevity
 
-  # Iterate over alignment lines
-  for line in a3m_lines.split( "\n" ):
-    if line.startswith( ">" ):
-      output.append( line )
-    elif len( line ) > 1:
-      line = list( line )
-      for pos, res in pos_res.items():
-        if line[ pos ] in "ACDEFGHIKLMNPQRSTVWY":
-          line[ pos ] = res
-      output.append( "".join( line ) ) 
-    else:
-      output.append( line )
-  return "\n".join( output )
 
-def mutate( x, y ): mutate_msa( x, y ) # Alias for brevity
+def plddt_to_bfactor(filename: str, maxval: float = 100.0) -> NoReturn:
+    r"""Converts a pLDDT vals to a B factor
+    This equation is derived from the following publication:
+    "Improved protein structure refinement guided by deep learning based
+    accuracy estimation" by Hiranuma et al 2021
+    https://doi.org/10.1038/s41467-021-21511-x
 
-def plddt_to_bfactor(
-    filename: str,
-    maxval: float = 100.
-  ) -> NoReturn:
-  r""" Converts a pLDDT vals to a B factor
-  This equation is derived from the following publication:
-  "Improved protein structure refinement guided by deep learning based
-  accuracy estimation" by Hiranuma et al 2021
-  https://doi.org/10.1038/s41467-021-21511-x
+    Parameters
+    ----------
+    filename : Name of PDB file
+    maxval : Set to 100 if using AF2 (or 1 if RoseTTAFold)
 
-  Parameters
-  ----------
-  filename : Name of PDB file
-  maxval : Set to 100 if using AF2 (or 1 if RoseTTAFold)
+    Returns
+    ----------
+    None
 
-  Returns
-  ----------
-  None
+    """
+    pdb = Bio.PDB.PDBParser().get_structure("TEMP", filename)
+    for atom in pdb.get_atoms():
+        rmsf = 1.5 * np.exp(4 * (0.7 - (atom.bfactor / maxval)))
+        atom.bfactor = (8.0 / 3.0) * (np.pi**2) * (rmsf**2)
 
-  """
-  pdb = Bio.PDB.PDBParser().get_structure( "TEMP", filename )
-  for atom in pdb.get_atoms():
-    rmsf = 1.5 * np.exp( 4 * ( 0.7 - ( atom.bfactor / maxval ) ) )
-    atom.bfactor = ( 8. / 3. ) * ( np.pi ** 2 ) * ( rmsf ** 2 )
-
-  pdbio = Bio.PDB.PDBIO()
-  pdbio.set_structure( pdb )
-  pdbio.save( filename )
+    pdbio = Bio.PDB.PDBIO()
+    pdbio.set_structure(pdb)
+    pdbio.save(filename)
