fbb8f78f93a459476d38bfeebfff5550ace8755d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 08:40:14 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/mmseqs2.py
+++ scripts/mmseqs2.py
@@ -341,6 +341,25 @@ class MMSeqs2Runner:
         # extract a3m files
         if not os.path.isfile(os.path.join(self.path, a3m_files[0])):
             with tarfile.open(self.tarfile) as tar_gz:
-                tar_gz.extractall(self.path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar_gz, self.path)
 
         return self._process_alignment(a3m_files, templates)
